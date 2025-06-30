4364abe7a69272a7e9d73db23f32f69bd2e6bce0
Rudra-Sankha-Sinhamahapatra <rssmp120@gmail.com>
2025-06-30 23:21:24 +0530

    adding

--- src/models/requests.rs
+++ src/models/requests.rs
@@ -3,43 +3,43 @@ use serde::Deserialize;
 #[derive(Deserialize)]
 pub struct CreateTokenRequest {
     #[serde(rename = "mintAuthority")]
-    pub mint_authority: String,
-    pub mint: String,
-    pub decimals: u8,
+    pub mint_authority: Option<String>,
+    pub mint: Option<String>,
+    pub decimals: Option<u8>,
 }
 
 #[derive(Deserialize)]
 pub struct MintTokenRequest {
-    pub mint: String,
-    pub destination: String,
-    pub authority: String,
-    pub amount: u64,
+    pub mint: Option<String>,
+    pub destination: Option<String>,
+    pub authority: Option<String>,
+    pub amount: Option<u64>,
 }
 
 #[derive(Deserialize)]
 pub struct SignMessageRequest {
-    pub message: String,
-    pub secret: String,
+    pub message: Option<String>,
+    pub secret: Option<String>,
 }
 
 #[derive(Deserialize)]
 pub struct VerifyMessageRequest {
-    pub message: String,
-    pub signature: String,
-    pub pubkey: String,
+    pub message: Option<String>,
+    pub signature: Option<String>,
+    pub pubkey: Option<String>,
 }
 
 #[derive(Deserialize)]
 pub struct SendSolRequest {
-    pub from: String,
-    pub to: String,
-    pub lamports: u64,
+    pub from: Option<String>,
+    pub to: Option<String>,
+    pub lamports: Option<u64>,
 }
 
 #[derive(Deserialize)]
 pub struct SendTokenRequest {
-    pub destination: String,
-    pub mint: String,
-    pub owner: String,
-    pub amount: u64,
+    pub destination: Option<String>,
+    pub mint: Option<String>,
+    pub owner: Option<String>,
+    pub amount: Option<u64>,
 } 
